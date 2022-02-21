c561494ea83f2fd850f2f438a404003afad68e8c
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2022-02-21 09:35:51 -0500

    initialize spring project

---
+++ src/test/java/com/example/springTutorial/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
